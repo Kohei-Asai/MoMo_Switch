0484e17f43760839b4c6faf4cf7e51ed964a050a
Kohei ASAI <ko2001he@gmail.com>
2022-06-27 13:59:42 +0900

    プログレスバーを共通化

--- window_device.py
+++ window_device.py
@@ -31,22 +31,22 @@ class App(tk.Frame):
         self.label_brush.grid(column=0, row=0)
         
         self.progress_brush = tk.IntVar(0)
-        self.pbar_brush = ttk.Progressbar(
-            self.frame1,
-            orient=tk.HORIZONTAL,
-            variable=self.progress_brush,
-            maximum=4,
-            length=200,
-            mode="determinate"
-        )
-        self.pbar_brush.grid(column=1, row=0)
+        # self.pbar_brush = ttk.Progressbar(
+        #     self.frame1,
+        #     orient=tk.HORIZONTAL,
+        #     variable=self.progress_brush,
+        #     maximum=4,
+        #     length=200,
+        #     mode="determinate"
+        # )
+        # self.pbar_brush.grid(column=1, row=0)
         
         self.button_brush = ttk.Button(
             self.frame1,
             text="brush",
             command=self.button_brush
         )
-        self.button_brush.grid(column=2, row=0)
+        self.button_brush.grid(column=1, row=0)
         
         self.brush_completed = tk.StringVar()
         self.brush_completed.set("Not yet")
@@ -54,7 +54,7 @@ class App(tk.Frame):
             self.frame1,
             textvariable=self.brush_completed
         )
-        self.label_brush_completed.grid(column=3, row=0)
+        self.label_brush_completed.grid(column=2, row=0)
         
         # drink
         
@@ -65,22 +65,22 @@ class App(tk.Frame):
         self.label_drink.grid(column=0, row=1)
         
         self.progress_drink = tk.IntVar(0)
-        self.pbar_drink = ttk.Progressbar(
-            self.frame1,
-            orient=tk.HORIZONTAL,
-            variable=self.progress_drink,
-            maximum=4,
-            length=200,
-            mode="determinate"
-        )
-        self.pbar_drink.grid(column=1, row=1)
+        # self.pbar_drink = ttk.Progressbar(
+        #     self.frame1,
+        #     orient=tk.HORIZONTAL,
+        #     variable=self.progress_drink,
+        #     maximum=4,
+        #     length=200,
+        #     mode="determinate"
+        # )
+        # self.pbar_drink.grid(column=1, row=1)
         
         self.button_drink = ttk.Button(
             self.frame1,
             text="drink",
             command=self.button_drink
         )
-        self.button_drink.grid(column=2, row=1)
+        self.button_drink.grid(column=1, row=1)
         
         self.drink_completed = tk.StringVar()
         self.drink_completed.set("Not yet")
@@ -88,7 +88,7 @@ class App(tk.Frame):
             self.frame1,
             textvariable=self.drink_completed
         )
-        self.label_drink_completed.grid(column=3, row=1)
+        self.label_drink_completed.grid(column=2, row=1)
         
         # senobi
         
@@ -99,22 +99,22 @@ class App(tk.Frame):
         self.label_senobi.grid(column=0, row=2)
         
         self.progress_senobi = tk.IntVar(0)
-        self.pbar_senobi = ttk.Progressbar(
-            self.frame1,
-            orient=tk.HORIZONTAL,
-            variable=self.progress_senobi,
-            maximum=4,
-            length=200,
-            mode="determinate"
-        )
-        self.pbar_senobi.grid(column=1, row=2)
+        # self.pbar_senobi = ttk.Progressbar(
+        #     self.frame1,
+        #     orient=tk.HORIZONTAL,
+        #     variable=self.progress_senobi,
+        #     maximum=4,
+        #     length=200,
+        #     mode="determinate"
+        # )
+        # self.pbar_senobi.grid(column=1, row=2)
         
         self.button_senobi = ttk.Button(
             self.frame1,
             text="senobi",
             command=self.button_senobi
         )
-        self.button_senobi.grid(column=2, row=2)
+        self.button_senobi.grid(column=1, row=2)
         
         self.senobi_completed = tk.StringVar()
         self.senobi_completed.set("Not yet")
@@ -122,7 +122,7 @@ class App(tk.Frame):
             self.frame1,
             textvariable=self.senobi_completed
         )
-        self.label_senobi_completed.grid(column=3, row=2)
+        self.label_senobi_completed.grid(column=2, row=2)
         
         # walk
         
@@ -133,22 +133,22 @@ class App(tk.Frame):
         self.label_walk.grid(column=0, row=3)
         
         self.progress_walk = tk.IntVar(0)
-        self.pbar_walk = ttk.Progressbar(
-            self.frame1,
-            orient=tk.HORIZONTAL,
-            variable=self.progress_walk,
-            maximum=4,
-            length=200,
-            mode="determinate"
-        )
-        self.pbar_walk.grid(column=1, row=3)
+        # self.pbar_walk = ttk.Progressbar(
+        #     self.frame1,
+        #     orient=tk.HORIZONTAL,
+        #     variable=self.progress_walk,
+        #     maximum=4,
+        #     length=200,
+        #     mode="determinate"
+        # )
+        # self.pbar_walk.grid(column=1, row=3)
         
         self.button_walk = ttk.Button(
             self.frame1,
             text="walk",
             command=self.button_walk
         )
-        self.button_walk.grid(column=2, row=3)
+        self.button_walk.grid(column=1, row=3)
         
         self.walk_completed = tk.StringVar()
         self.walk_completed.set("Not yet")
@@ -156,7 +156,7 @@ class App(tk.Frame):
             self.frame1,
             textvariable=self.walk_completed
         )
-        self.label_walk_completed.grid(column=3, row=3)
+        self.label_walk_completed.grid(column=2, row=3)
         
         # face
         
@@ -167,22 +167,22 @@ class App(tk.Frame):
         self.label_face.grid(column=0, row=4)
         
         self.progress_face = tk.IntVar(0)
-        self.pbar_face = ttk.Progressbar(
-            self.frame1,
-            orient=tk.HORIZONTAL,
-            variable=self.progress_face,
-            maximum=4,
-            length=200,
-            mode="determinate"
-        )
-        self.pbar_face.grid(column=1, row=4)
+        # self.pbar_face = ttk.Progressbar(
+        #     self.frame1,
+        #     orient=tk.HORIZONTAL,
+        #     variable=self.progress_face,
+        #     maximum=4,
+        #     length=200,
+        #     mode="determinate"
+        # )
+        # self.pbar_face.grid(column=1, row=4)
         
         self.button_face = ttk.Button(
             self.frame1,
             text="face",
             command=self.button_face
         )
-        self.button_face.grid(column=2, row=4)
+        self.button_face.grid(column=1, row=4)
         
         self.face_completed = tk.StringVar()
         self.face_completed.set("Not yet")
@@ -190,7 +190,32 @@ class App(tk.Frame):
             self.frame1,
             textvariable=self.face_completed
         )
-        self.label_face_completed.grid(column=3, row=4)
+        self.label_face_completed.grid(column=2, row=4)
+        
+        # 共通化したやつ
+        
+        self.label_all = ttk.Label(
+            self.frame1,
+            text="全部"
+        )
+        self.label_all.grid(column=0, row=5)
+
+        self.progress_all = tk.IntVar(0)
+        self.pbar_all = ttk.Progressbar(
+            self.frame1,
+            orient=tk.HORIZONTAL,
+            variable=self.progress_all,
+            maximum=20,
+            length=200,
+            mode="determinate"
+        )
+        self.pbar_all.grid(column=1, row=5)
+        
+        self.button_all = ttk.Button(
+            self.frame1,
+            text="全部",
+        )
+        self.button_all.grid(column=2, row=5)
         
         # frame2
         
@@ -221,12 +246,14 @@ class App(tk.Frame):
         )
 
     def button_brush(self):
-        self.progress_brush.set(self.progress_brush.get() + 1)
+        if self.progress_brush.get() < 4:
+            self.progress_brush.set(self.progress_brush.get() + 1)
+            self.progress_all.set(self.progress_all.get() + 1)
             
         if self.progress_brush.get() >= 4:
             self.brush_completed.set("Task Completed!")
             self.canvas.itemconfig(self.show_image, image=self.img_finish)
-            playsound("sounds/jenga.mp3")
+            # playsound("sounds/jenga.mp3")
         elif  1 <= self.progress_brush.get() <= 3:
             self.brush_completed.set("Doing......")
             self.canvas.itemconfig(self.show_image, image=self.img_brush)
@@ -235,12 +262,14 @@ class App(tk.Frame):
             self.canvas.itemconfig(self.show_image, image=self.img_sleep)
             
     def button_drink(self):
-        self.progress_drink.set(self.progress_drink.get() + 1)
+        if self.progress_drink.get() < 4:
+            self.progress_drink.set(self.progress_drink.get() + 1)
+            self.progress_all.set(self.progress_all.get() + 1)
             
         if self.progress_drink.get() >= 4:
             self.drink_completed.set("Task Completed!")
             self.canvas.itemconfig(self.show_image, image=self.img_finish)
-            playsound("sounds/jenga.mp3")
+            # playsound("sounds/jenga.mp3")
         elif  1 <= self.progress_drink.get() <= 3:
             self.drink_completed.set("Doing......")
             self.canvas.itemconfig(self.show_image, image=self.img_drink)
@@ -249,12 +278,14 @@ class App(tk.Frame):
             self.canvas.itemconfig(self.show_image, image=self.img_sleep)
             
     def button_senobi(self):
-        self.progress_senobi.set(self.progress_senobi.get() + 1)
+        if self.progress_senobi.get() < 4:
+            self.progress_senobi.set(self.progress_senobi.get() + 1)
+            self.progress_all.set(self.progress_all.get() + 1)
             
         if self.progress_senobi.get() >= 4:
             self.senobi_completed.set("Task Completed!")
             self.canvas.itemconfig(self.show_image, image=self.img_finish)
-            playsound("sounds/jenga.mp3")
+            # playsound("sounds/jenga.mp3")
         elif  1 <= self.progress_senobi.get() <= 3:
             self.senobi_completed.set("Doing......")
             self.canvas.itemconfig(self.show_image, image=self.img_senobi)
@@ -263,12 +294,14 @@ class App(tk.Frame):
             self.canvas.itemconfig(self.show_image, image=self.img_sleep)
             
     def button_walk(self):
-        self.progress_walk.set(self.progress_walk.get() + 1)
+        if self.progress_walk.get() < 4:
+            self.progress_walk.set(self.progress_walk.get() + 1)
+            self.progress_all.set(self.progress_all.get() + 1)
             
         if self.progress_walk.get() >= 4:
             self.walk_completed.set("Task Completed!")
             self.canvas.itemconfig(self.show_image, image=self.img_finish)
-            playsound("sounds/jenga.mp3")
+            # playsound("sounds/jenga.mp3")
         elif  1 <= self.progress_senobi.get() <= 3:
             self.walk_completed.set("Doing......")
             self.canvas.itemconfig(self.show_image, image=self.img_walk)
@@ -277,12 +310,14 @@ class App(tk.Frame):
             self.canvas.itemconfig(self.show_image, image=self.img_walk)
             
     def button_face(self):
-        self.progress_face.set(self.progress_face.get() + 1)
+        if self.progress_face.get() < 4:
+            self.progress_face.set(self.progress_face.get() + 1)
+            self.progress_all.set(self.progress_all.get() + 1)
             
         if self.progress_face.get() >= 4:
             self.face_completed.set("Task Completed!")
             self.canvas.itemconfig(self.show_image, image=self.img_finish)
-            playsound("sounds/jenga.mp3")
+            # playsound("sounds/jenga.mp3")
         elif  1 <= self.progress_face.get() <= 3:
             self.face_completed.set("Doing......")
             self.canvas.itemconfig(self.show_image, image=self.img_face)
