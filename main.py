fca7b26ece69a81ce1ddcf2687201e34c93d0264
Kohei ASAI <ko2001he@gmail.com>
2022-06-29 15:19:46 +0900

    among usによせた

--- main.py
+++ main.py
@@ -8,15 +8,17 @@ import numpy as np
 import torch
 import arduino
 import classifier
+from ttkthemes import *
 
 class App(tk.Frame):
     def __init__(self, master = None):
         super().__init__(master)
 
         master.title("ウェアラブルデバイスの画面")
-        master.geometry("500x600")
+        master.geometry("600x300")
         
-        ttk.Style().theme_use('classic')
+        style = ttk.Style()
+        style.theme_use('black')
         
         # 変数
         
@@ -27,108 +29,33 @@ class App(tk.Frame):
         self.progress_face = tk.IntVar(0)
         self.progress_all = tk.IntVar(0)
         
+        frame = ttk.Frame(
+            master,
+            padding=10,
+            width=600,
+            height=300,
+        )
+        frame.grid(column=0, row=0, sticky=(tk.N, tk.S, tk.E, tk.W))
+        
         # frame1
         
         frame1 = ttk.Frame(
-            master,
+            frame,
             padding=10,
             width=400,
             height=250
         )
-        frame1.grid(column=0, row=0)
+        frame1.grid(column=0, row=0, sticky=(tk.N, tk.S, tk.E, tk.W))
         
-        # brush
-        
-        button_brush = ttk.Button(
-            frame1,
-            text="brush",
-            command=self.update_brush
-        )
-        button_brush.grid(column=0, row=0)
-        
-        self.brush_completed = tk.StringVar()
-        self.brush_completed.set("Not yet")
-        label_brush_completed = ttk.Label(
-            frame1,
-            textvariable=self.brush_completed
-        )
-        label_brush_completed.grid(column=1, row=0)
-        
-        # drink
-        
-        button_drink = ttk.Button(
-            frame1,
-            text="drink",
-            command=self.update_drink
-        )
-        button_drink.grid(column=0, row=1)
-        
-        self.drink_completed = tk.StringVar()
-        self.drink_completed.set("Not yet")
-        label_drink_completed = ttk.Label(
-            frame1,
-            textvariable=self.drink_completed
-        )
-        label_drink_completed.grid(column=1, row=1)
-        
-        # senobi
-        
-        button_senobi = ttk.Button(
-            frame1,
-            text="senobi",
-            command=self.update_senobi
-        )
-        button_senobi.grid(column=0, row=2)
-        
-        self.senobi_completed = tk.StringVar()
-        self.senobi_completed.set("Not yet")
-        label_senobi_completed = ttk.Label(
-            frame1,
-            textvariable=self.senobi_completed
-        )
-        label_senobi_completed.grid(column=1, row=2)
-        
-        # walk
-        
-        button_walk = ttk.Button(
-            frame1,
-            text="walk",
-            command=self.update_walk
-        )
-        button_walk.grid(column=0, row=3)
-        
-        self.walk_completed = tk.StringVar()
-        self.walk_completed.set("Not yet")
-        label_walk_completed = ttk.Label(
-            frame1,
-            textvariable=self.walk_completed
-        )
-        label_walk_completed.grid(column=1, row=3)
-        
-        # face
-        
-        button_face = ttk.Button(
-            frame1,
-            text="face",
-            command=self.update_face
-        )
-        button_face.grid(column=0, row=4)
-        
-        self.face_completed = tk.StringVar()
-        self.face_completed.set("Not yet")
-        label_face_completed = ttk.Label(
-            frame1,
-            textvariable=self.face_completed
-        )
-        label_face_completed.grid(column=1, row=4)
-        
-        # 共通化したやつ
+        # 進捗バー
         
         label_all = ttk.Label(
             frame1,
             text="進捗"
         )
-        label_all.grid(column=0, row=5)
+        label_all.grid(column=0, row=0)
+        
+        style.configure("red.Horizontal.TProgressbar", background='green2')
 
         pbar_all = ttk.Progressbar(
             frame1,
@@ -136,47 +63,110 @@ class App(tk.Frame):
             variable=self.progress_all,
             maximum=20,
             length=200,
-            mode="determinate"
+            mode="determinate",
+            style="red.Horizontal.TProgressbar"
         )
-        pbar_all.grid(column=1, row=5)
+        pbar_all.grid(column=1, row=0)
         
         # frame2
         
         frame2 = ttk.Frame(
-            master,
+            frame,
             padding=10,
             width=400,
             height=400
         )
-        frame2.grid(column=0, row=1)
+        frame2.grid(column=0, row=1, sticky=(tk.N, tk.S, tk.E, tk.W))
         
-        self.canvas = tk.Canvas(frame2)
-        self.canvas.grid(column=0, row=0)
-        self.img_nothing = ImageTk.PhotoImage(Image.open('images/sleep.png').resize((200,200)))
-        self.img_brush = ImageTk.PhotoImage(Image.open('images/brush.png').resize((200,200)))
-        self.img_drink = ImageTk.PhotoImage(Image.open('images/drink.png').resize((200,200)))
-        self.img_senobi = ImageTk.PhotoImage(Image.open('images/senobi.png').resize((200,200)))
-        self.img_walk = ImageTk.PhotoImage(Image.open('images/walk.png').resize((200,200)))
-        self.img_face = ImageTk.PhotoImage(Image.open('images/washface.png').resize((200,200)))
-        self.img_finish = ImageTk.PhotoImage(Image.open('images/finish.png').resize((200,200)))
-        self.update()
-        w = self.canvas.winfo_width()
-        h = self.canvas.winfo_height()
-        self.show_image = self.canvas.create_image(
-            w / 2,
-            h / 2,                   
-            image=self.img_nothing
+        # brush
+        
+        button_brush = ttk.Button(
+            frame2,
+            text="brush",
+            command=self.update_brush
         )
+        button_brush.grid(column=0, row=1)
         
-        # frame 3
+        self.label_brush_completed = ttk.Label(
+            frame2,
+            text="Washroom: Brush Teeth",
+            foreground="white",
+        )
+        self.label_brush_completed.grid(column=1, row=1)
+        
+        # drink
+        
+        button_drink = ttk.Button(
+            frame2,
+            text="drink",
+            command=self.update_drink
+        )
+        button_drink.grid(column=0, row=2)
+    
+        self.label_drink_completed = ttk.Label(
+            frame2,
+            text="Kitchen: Drink Water",
+            foreground="white",
+        )
+        self.label_drink_completed.grid(column=1, row=2)
+        
+        # senobi
+        
+        button_senobi = ttk.Button(
+            frame2,
+            text="senobi",
+            command=self.update_senobi
+        )
+        button_senobi.grid(column=0, row=3)
+        
+        self.label_senobi_completed = ttk.Label(
+            frame2,
+            text="Bedroom: Senobi",
+            foreground="white",
+        )
+        self.label_senobi_completed.grid(column=1, row=3)
+        
+        # walk
+        
+        button_walk = ttk.Button(
+            frame2,
+            text="walk",
+            command=self.update_walk
+        )
+        button_walk.grid(column=0, row=4)
+        
+        self.label_walk_completed = ttk.Label(
+            frame2,
+            text="EveryWhere: Walk",
+            foreground="white",
+        )
+        self.label_walk_completed.grid(column=1, row=4)
+        
+        # face
+        
+        button_face = ttk.Button(
+            frame2,
+            text="face",
+            command=self.update_face
+        )
+        button_face.grid(column=0, row=5)
+        
+        self.label_face_completed = ttk.Label(
+            frame2,
+            text="Washroom: Wash Face",
+            foreground="white",
+        )
+        self.label_face_completed.grid(column=1, row=5)
+        
+        # frame3
         
         frame3 = ttk.Frame(
-            master,
+            frame,
             padding=10,
             width=400,
             height=400
         )
-        frame3.grid(column=0, row=2)
+        frame3.grid(column=1, row=0, sticky=(tk.N, tk.S, tk.E, tk.W))
         
         button_start = ttk.Button(
             frame3,
@@ -198,56 +188,87 @@ class App(tk.Frame):
             command=self.create_dialog_graph
         )
         button_report.grid(column=2, row=0)
+        
+        # frame4
+        
+        frame4 = ttk.Frame(
+            frame,
+            padding=10,
+            width=400,
+            height=400
+        )
+        frame4.grid(column=1, row=1, sticky=(tk.N, tk.S, tk.E, tk.W))
+        
+        self.canvas = tk.Canvas(
+            frame4,
+            background='gray35'
+        )
+        self.canvas.grid(column=0, row=0)
+        self.img_nothing = ImageTk.PhotoImage(Image.open('images/sleep.png').resize((200,200)))
+        self.img_brush = ImageTk.PhotoImage(Image.open('images/brush.png').resize((200,200)))
+        self.img_drink = ImageTk.PhotoImage(Image.open('images/drink.png').resize((200,200)))
+        self.img_senobi = ImageTk.PhotoImage(Image.open('images/senobi.png').resize((200,200)))
+        self.img_walk = ImageTk.PhotoImage(Image.open('images/walk.png').resize((200,200)))
+        self.img_face = ImageTk.PhotoImage(Image.open('images/washface.png').resize((200,200)))
+        self.img_finish = ImageTk.PhotoImage(Image.open('images/finish.png').resize((200,200)))
+        self.update()
+        w = self.canvas.winfo_width()
+        h = self.canvas.winfo_height()
+        self.show_image = self.canvas.create_image(
+            w / 2,
+            h / 2,                   
+            image=self.img_nothing
+        )
 
     def update_brush(self):
         if self.progress_brush.get() >= 4:
-            self.brush_completed.set("Task Completed!")
+            self.label_brush_completed["foreground"] = "lime green"
             self.canvas.itemconfig(self.show_image, image=self.img_finish)
         else:
             self.progress_brush.set(self.progress_brush.get() + 1)
             self.progress_all.set(self.progress_all.get() + 1)
-            self.brush_completed.set("Doing......")
             self.canvas.itemconfig(self.show_image, image=self.img_brush)
+            self.label_brush_completed["foreground"] = "yellow"
             
     def update_drink(self):
         if self.progress_drink.get() >= 4:
-            self.drink_completed.set("Task Completed!")
+            self.label_drink_completed["foreground"] = "lime green"
             self.canvas.itemconfig(self.show_image, image=self.img_finish)
         else:
             self.progress_drink.set(self.progress_drink.get() + 1)
             self.progress_all.set(self.progress_all.get() + 1)
-            self.drink_completed.set("Doing......")
             self.canvas.itemconfig(self.show_image, image=self.img_drink)
+            self.label_drink_completed["foreground"] = "yellow"
             
     def update_senobi(self):
         if self.progress_senobi.get() >= 4:
-            self.senobi_completed.set("Task Completed!")
+            self.label_senobi_completed["foreground"] = "lime green"
             self.canvas.itemconfig(self.show_image, image=self.img_finish)
         else:
             self.progress_senobi.set(self.progress_senobi.get() + 1)
             self.progress_all.set(self.progress_all.get() + 1)
-            self.senobi_completed.set("Doing......")
-            self.canvas.itemconfig(self.show_image, image=self.img_senobi)
+            self.canvas.itemconfig(self.show_image, image=self.img_senobi),
+            self.label_senobi_completed["foreground"] = "yellow"
             
     def update_walk(self):
         if self.progress_walk.get() >= 4:
-            self.walk_completed.set("Task Completed!")
+            self.label_walk_completed["foreground"] = "lime green"
             self.canvas.itemconfig(self.show_image, image=self.img_finish)
         else:
             self.progress_walk.set(self.progress_walk.get() + 1)
             self.progress_all.set(self.progress_all.get() + 1)
-            self.walk_completed.set("Doing......")
             self.canvas.itemconfig(self.show_image, image=self.img_walk)
+            self.label_walk_completed["foreground"] = "yellow"
             
     def update_face(self):
         if self.progress_face.get() >= 4:
-            self.face_completed.set("Task Completed!")
+            self.label_face_completed["foreground"] = "lime green"
             self.canvas.itemconfig(self.show_image, image=self.img_finish)
         else:
             self.progress_face.set(self.progress_face.get() + 1)
             self.progress_all.set(self.progress_all.get() + 1)
-            self.face_completed.set("Doing......")
             self.canvas.itemconfig(self.show_image, image=self.img_face)
+            self.label_face_completed["foreground"] = "yellow"
             
     def update_nothing(self):
         self.canvas.itemconfig(self.show_image, image=self.img_nothing)
@@ -255,7 +276,7 @@ class App(tk.Frame):
     def create_dialog_graph(self):
         dlg = tk.Toplevel(self)
         dlg.title("今日の結果")
-        dlg.geometry("700x500")
+        dlg.geometry("1000x800")
         
         fig = plt.figure()
         ax = fig.add_subplot()
@@ -304,7 +325,7 @@ class App(tk.Frame):
         self.after_cancel(self.jobID)
 
 if __name__ == "__main__":
-    root = tk.Tk()
+    root = ThemedTk()
     model = classifier.load_model(
         model_path='model_9freedom.pth',
         input_dim=9,
